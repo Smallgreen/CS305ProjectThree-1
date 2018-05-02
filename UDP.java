7a53035d0db8abb9a4178a347d9de3720318f968
wangme <wangme@lafayette.edu>
2018-05-02 19:51:37 -0400

    send msg

--- UDP.java
+++ UDP.java
@@ -18,12 +18,47 @@ public class UDP {
             msg(the actual message with the concatenated ip addresses and port numbers)
  */
     public void sendMessage(String msg, Neighbor dest) throws IOException{
-        dest.getIp();
+        //calculate new msg size = msg length + next hop's ip + port
+        int length = 0;
+        byte splitter = 120;
+        String address = dest.getIp() + "-" + Integer.toString(dest.getPort());
+
+        length = msg.length() + 1 + address.length();
+
+        byte[] sentMsg = new byte[length];
+
+        int curIndex = 0;
+
+        //fill in the contents
+        byte[] msgContent = msg.getBytes();
+        for(int i = 0; i < msg.length(); i++){
+            sentMsg[i] = msgContent[i];
+        }
+
+        sentMsg[msg.length()] = splitter;
+
+        byte[] add = address.getBytes();
+        int temp = 0;
+        for(int i = msg.length() + 1; i < curIndex + 1 + address.length(); i++){
+            sentMsg[i] = add[temp];
+            temp++;
+        }
+
+        sendHelper(sentMsg, dest.getIp(), dest.getPort());
+
 
     }
 
-    public void sendHelper(byte[] msg, SocketAddress dest){
-
+    public void sendHelper(byte[] msg, String ip, int port){
+        try {
+            InetAddress ipAdress = InetAddress.getByName(ip);
+            DatagramPacket packet = new DatagramPacket(msg, msg.length, ipAdress, port);
+            srcSocket.send(packet);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
